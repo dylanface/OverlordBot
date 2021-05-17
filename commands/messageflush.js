dc99327b5279f71aede5daf6135b494d7f14814d
Dylanface <dylanbmxer1234@gmail.com>
2021-05-16 22:29:15 -0700

    🙈 Fixed Accidental Deletion of Necessary Node Modules

---
+++ commands/messageflush.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'userindex',
+    description: "This can ban someone before they join the discord!",
+    guildOnly: "true",
+    args: 1,
+    permissions: 'BAN_MEMBERS',
+    //guildExclusive: "140247578242580481",
+    cooldown: 10,
+    async execute(client, message, args, Discord) {
+        
+    },
+};
