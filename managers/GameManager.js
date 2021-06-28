8c58579068277a2c80eed8a0c9e417ac479b9439
Dylanface <dylanbmxer1234@gmail.com>
2021-06-28 00:45:46 -0700

    ☎ GameManager additions
    Co-authored-by: Alteram1 <Alteram1@users.noreply.github.com>

--- managers/GameManager.js
+++ managers/GameManager.js
@@ -6,63 +6,110 @@ module.exports = async (client, Discord) => {
     /*
     *    Represents an instance of any game within Overlord
     *    @param {object} initiatingUser The User who started the game instance
-    *    @param {UUID} gameUUID A unique class generated UUID representing this game instance
     *    @param {integer} gameNumber The order number of the gameMode game instance list
     *    @param {string} gameType The game type this instance represents
     *    @param {collection} challengers The challenged User(s) if any
+    *    @param {collection} challengers The challenged User(s) if any
     */
     class GameInstance {
-        constructor(initiatingUser, gameNumber, gameType, challengers, modifiers) {
+      constructor(
+        initiatingUser,
+        gameNumber,
+        gameType,
+        challengers,
+        modifiers
+      ) {
+        this.initiatingUser = initiatingUser;
+        this.gameNumber = gameNumber;
+        this.gameType = gameType;
+        this.dateOfCreation = new Date();
+        this.challengers = challengers;
+        this.modifiers = modifiers;
+        this.players = new Discord.Collection();
+        this.mods = new Discord.Collection();
+        this.rewards = new Discord.Collection();
+        this.gameState = 'Startup';
 
-            this.initiatingUser = initiatingUser;
-            this.gameNumber = gameNumber;
-            this.gameType = gameType;
-            this.dateOfCreation = new Date()
-            this.challengers = challengers;
-            this.modifiers = modifiers;
-            this.players = new Discord.Collection()
-            this.mods = new Discord.Collection()
-            this.rewards = new Discord.Collection()
+        this.gameUUID = new uuidv4();
+      }
+
+      addPlayer(id) {
+        const user = await this.players.get(id);
+        if (user) return user;
+        else {
+          let user = await this.players.set(id, new Discord.Collection());
+          return GameInstance;
+        }
+      }
+      
+      removePlayer(id) {
+        const user = await this.players.get(id);
+        if (user) this.players.delete(id);
+        return GameInstance;
+      }
+
+      getPlayer(id) {
+        return this.players.get(id);
+      }
+
+      getPlayers() {
+        return this.players;
+      }
+
+      addPlayerScore(id, amount) {
+        let playerScore = await this.players.get(id);
+        if (playerScore) playerScore += amount;
+        else {
+          playerScore = this.addPlayer(id);
+          playerScore += amount;
+        }
+        return GameInstance;
+      }
+
+      //TODO: getLeaderboard(amount)? rank players scores in the game instance
+      //if amount between numbers: display number of users on leaderboard
+
+      addMod(id) {
+        const mod = await this.mods.get(id);
+        if (mod) return mod;
+        else {
+          let mod = await this.mods.set(id, new Discord.Collection());
+          return mod;
+        }
+      }
+
+      clearModifiers() {
+        this.modifiers.clear();
+        return GameInstance
+      }
+
+      setGameType(gameType) {
+        this.gameType = gameType;
+        return GameInstance;
+      }
+
+      addCoinsToPot(id, amount) {
+        let userRewards = await this.rewards.get(id);
+        if (userRewards) {
+          userRewards += amount;
+        } else {
+            this.rewards.set(id, amount);
+        }
+        client.currency.add(id, -amount)
+        return GameInstance
+      }
             
-            this.gameUUID = new uuidv4();
-        }
+      changeGameState(gameState) {
+          this.gameState = gameState;
+          return GameInstance;
+      } 
+      
+      
+      //TODO: getCoinsInPot
 
-        addPlayer(id) {
-            const user = await this.players.get(id);
-		    if (user) return user;
-		    else {
-                let user = await this.players.set(id, new Discord.Collection())
-                return user;
-            }
-        }
+      //TODO: Assigning rewards
+      //getWinners(#OfWinners?)
 
-        addPlayerScore(id, amount) {
-            let user = await this.players.get(id);
-            if (user) user.points += amount
-            else {
-                user = this.addPlayer(id)
-                user.points += amount
-            }
-        }
-
-        addMod(id) {
-            const mod = await this.mods.get(id);
-		    if (mod) return mod;
-		    else {
-                let mod = await this.mods.set(id, new Discord.Collection())
-                return mod;
-            }
-        }
-
-        clearModifiers() {
-            this.modifiers.clear()
-        }
-
-        setGameType(gameType) {
-            this.gameType = gameType
-        }
-
-        
 
     }
 
