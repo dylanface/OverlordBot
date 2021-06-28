971b603164f53ea5f9b48c1982a0e6c3c5a948f5
Alteram1 <86462878+Alteram1@users.noreply.github.com>
2021-06-27 23:58:12 -0700

    Co-authored-by: Dylanbmxer <Dylanbmxer@users.noreply.github.com>

--- managers/BingoManager.js
+++ managers/BingoManager.js
@@ -1,59 +1,130 @@
-const bingo = client.games.bingo
+const Discord = require('discord.js');
+const myIntents = Discord.Intents.ALL;
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"], intents: myIntents });
 
+//const bingo = client.games.bingo
 
+const { GameInstance } = require('./GameManager');
 
-const mockBoard = [
-    [0,5,10,15,20],
-    [1,6,11,16,21],
-    [2,7,null,17,22],
-    [3,8,13,18,23],
-    [4,9,14,19,24],
-]
+// const game = new GameInstance()
+const generatedPairs = new Discord.Collection();
+const drawnPairs = new Discord.Collection();
 
-async function newCard(user, amount, gameNumber){
-    
-    var usedNums = new Array(76); 
-    
-    
-    const column = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4)
-
-    for (let i=0 ; i<24 ; i++){
-        if (i == 12){
-            setFreeSpace();
-            continue;
-        }
-
-        let square = column[i] * 15;
+generatePairs()
+callNumber()
+    async function prepareMatch() {
         
-        do {
-           var newNum = square + Math.floor(Math.random() * 15) + 1;
-        }
-        while (usedNums[newNum]); 
+        
+
+        // .randomKey(amount)
+        // Obtains unique random key(s) from this collection.
+        // Returns: * or Array<*>
+        // A single key if no amount is provided or an array
     }
-}
+    
+    async function generatePairs() {
+        var bingoLetter;
+        for (let i=1; i<=75; i++) {
+            if (i <= 15) {
 
-/*      From Draw.js drawing numbers:
-        const rand;
-        do {
-            rand = Math.floor(Math.random() * 75)+1;
+                bingoLetter = 'B';
+            } // Add letter before number
+            else if (i > 15 && i <= 30) {//Do you even need the >'s here? It won't get this far if it's not >15...?
+                bingoLetter = 'I';
+            }
+            else if (i > 30 && i <= 45) {
+                bingoLetter = 'N';
+            }
+            else if (i > 45 && i <= 60) {
+                bingoLetter = 'G';
+            }
+            else {
+                bingoLetter = 'O';
+            }
+            generatedPairs.set(i, bingoLetter);
         }
-        while (client.games.bingo.calledNumbers.includes(rand));
-
-
-        matching up the current "const column" numbers with multiplied by 15, 
-        and then adding in random 1-15 to that number in theory should get you 1-75?
-
-        I was just thinking, it bugs me a bit that the draw script does random 1-75, 
-        and the bingomanger wants to do random 1-15 plus column*15...
+        console.log(generatedPairs)
+    }
+    // TODO Finish: Call bingo numbers
+    async function callNumber() {
+        //randomKey fetches the number
+        //random fetched the letter
+        let key = await generatedPairs.randomKey()
+        let value = await generatedPairs.get(key)
+        generatedPairs.delete(key)
+        drawnPairs.set(key, value)
+        console.log(drawnPairs)
+        if (generatedPairs.has(key)) return console.log(`Uh oh a key has not been recorded properly`)
+        //remove key/value from generatedPairs and add to drawnPairs...
         
-        Makes sense in a way, less randomness I guess, cause you'll always pull 
-        a number from the column you want to fill.
+    }
 
-
-*/
-
-
-async function setFreeSpace() {
-
-
-}
+    
+    // TODO Checking user's board vs drawn pairs.
+    
+    //array of numbers, and current called number
+    //check bingo up till current called number
+    
+    
+    const mockBoard = [
+        [0,5,10,15,20],
+        [1,6,11,16,21],
+        [2,7,null,17,22],
+        [3,8,13,18,23],
+        [4,9,14,19,24],
+    ]
+    
+    const emptyBoard = [
+        [``,``,``,``,``],
+        [``,``,``,``,``],
+        [``,``,`Free Space`,``,``],
+        [``,``,``,``,``],
+        [``,``,``,``,``],
+    ]
+    
+    async function newCard(user, amount, gameNumber){
+        
+        var usedNums = new Array(76); 
+        
+        
+        const column = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4)
+        
+        for (let i=0 ; i<24 ; i++){
+            if (i == 12){
+                setFreeSpace();
+                continue;
+            }
+            
+            let square = column[i] * 15;
+            
+            do {
+                var newNum = square + Math.floor(Math.random() * 15) + 1;
+            }
+            while (usedNums[newNum]); 
+        }
+    }
+    
+    /*      From Draw.js drawing numbers:
+    const rand;
+    do {
+        rand = Math.floor(Math.random() * 75)+1;
+    }
+    while (client.games.bingo.calledNumbers.includes(rand));
+    
+    
+    matching up the current "const column" numbers with multiplied by 15, 
+    and then adding in random 1-15 to that number in theory should get you 1-75?
+    
+    I was just thinking, it bugs me a bit that the draw script does random 1-75, 
+    and the bingomanger wants to do random 1-15 plus column*15...
+    
+    Makes sense in a way, less randomness I guess, cause you'll always pull 
+    a number from the column you want to fill.
+    
+    
+    */
+   
+   
+   async function setFreeSpace() {
+       
+   }
--- managers/GameManager.js
+++ managers/GameManager.js
@@ -1,8 +1,116 @@
-module.export = (client) => {
-client.games.bingo = new Discord.Collection();
-client.games.bingo.calledNumbers = new Array();
-client.games.bingo.gameNumber = new Number(); //?
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
-client.games.ticTacToe = new Discord.Collection();
-client.games.ticTacToe.activeGame = new Boolean();
+module.exports = async (client, Discord) => {
+
+    /*
+    *    Represents an instance of any game within Overlord
+    *    @param {object} initiatingUser The User who started the game instance
+    *    @param {integer} gameNumber The order number of the gameMode game instance list
+    *    @param {string} gameType The game type this instance represents
+    *    @param {collection} challengers The challenged User(s) if any
+    *    @param {collection} challengers The challenged User(s) if any
+    */
+    class GameInstance {
+        constructor(
+            initiatingUser,
+            gameNumber,
+            gameType,
+            challengers,
+            modifiers
+        ) {
+            this.initiatingUser = initiatingUser;
+            this.gameNumber = gameNumber;
+            this.gameType = gameType;
+            this.dateOfCreation = new Date();
+            this.challengers = challengers;
+            this.modifiers = modifiers;
+            this.players = new Discord.Collection();
+            this.mods = new Discord.Collection();
+            this.rewards = new Discord.Collection();
+            this.gameState = 'Startup';
+
+            this.gameUUID = new uuidv4();
+        }
+
+        async addPlayer(id) {
+            const user = await this.players.get(id);
+            if (user) return user;
+            else {
+                let user = await this.players.set(id, new Discord.Collection());
+                return user;
+            }
+        }
+
+        async removePlayer(id) {
+            const user = await this.players.get(id);
+            if (user) this.players.delete(id);
+            return this.players;
+        }
+
+        getPlayer(id) {
+            return this.players.get(id);
+        }
+
+        getPlayers() {
+            return this.players;
+        }
+
+        async addPlayerScore(id, amount) {
+            let playerScore = await this.players.get(id);
+            if (playerScore) playerScore += amount;
+            else {
+                playerScore = this.addPlayer(id);
+                playerScore += amount;
+            }
+            return playerScore;
+        }
+
+        //TODO: getLeaderboard(amount)? rank players scores in the game instance
+        //if amount between numbers: display number of users on leaderboard
+
+        async addMod(id) {
+            const mod = await this.mods.get(id);
+            if (mod) return mod;
+            else {
+                let mod = await this.mods.set(id, new Discord.Collection());
+                return mod;
+            }
+        }
+
+        clearModifiers() {
+            this.modifiers.clear();
+            return this.modifiers;
+        }
+
+        setGameType(gameType) {
+            this.gameType = gameType;
+            return this.gameType;
+        }
+
+        async addCoinsToPot(id, amount) {
+            let userRewards = await this.rewards.get(id);
+            if (userRewards) {
+                userRewards += amount;
+            } else {
+                this.rewards.set(id, amount);
+            }
+            client.currency.add(id, -amount)
+        }
+
+        changeGameState(gameState) {
+            this.gameState = gameState;
+            return this.gameState;
+        }
+
+
+        //TODO: getCoinsInPot
+
+        //TODO: Assigning rewards
+        //getWinners(#OfWinners?)
+
+
+    }
+
+
 }
