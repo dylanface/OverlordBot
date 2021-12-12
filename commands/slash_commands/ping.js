b24f57ccc54aa83546e9612f079cff2365f02ef5
Dylanface <dylanbmxer1234@gmail.com>
2021-12-12 10:21:10 -0800

    Co-authored-by: Alteram <Alteram1@users.noreply.github.com>

--- commands/slash_commands/ping.js
+++ commands/slash_commands/ping.js
@@ -15,18 +15,6 @@ module.exports = {
     async execute(interaction, client) {
 		await interaction.deferReply();
 
-		// const fakeManager = new StatisticManager(null, 'json_local', client);
-		// setTimeout(async () => {
-		// 	await fakeManager.updateStatsCache('673339828590608403', { total: 10, correct: 5, incorrect: 5 });
-
-		// 	await fakeManager.saveStatsLocal();
-		// } , 10000);
-		// setTimeout(() => {
-
-		// 	fakeManager.fetchStats()
-		// }, 10000)
-
-
 		const fakeManager = await client.chatGameManager.getGuildChatGameManager(interaction.guildId);
 		setTimeout(() => {
 			fakeManager.beginChallenge()
